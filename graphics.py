9e4412c65510cba892524c63a1eb87a326fd1c48
RickSweetness <rrheubottom@gmail.com>
2025-03-27 00:39:03 -0400

    added line and point classes

--- graphics.py
+++ graphics.py
@@ -21,3 +21,21 @@ class Window:
     
     def close(self):
         self.__is_running = False
+    
+    def draw_line(self, line, fill_colour):
+        line.draw(self.__canvas, fill_colour)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    
+    def draw(self, canvas, fill_colour):
+        canvas.create_line(
+    self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_colour, width=2
+)        
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from window import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0, 0), Point(400, 300)), "#F4F4F4")
     win.wait_for_close()
 
 main()
