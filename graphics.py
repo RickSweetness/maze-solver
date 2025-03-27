b735440d74b43dbefb1b2fbdd45deac7845f7bbd
RickSweetness <rrheubottom@gmail.com>
2025-03-27 10:32:53 -0400

    Added solution drawing logic

--- graphics.py
+++ graphics.py
@@ -38,4 +38,45 @@ class Line:
     def draw(self, canvas, fill_colour):
         canvas.create_line(
     self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_colour, width=2
-)        
+)
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "#F4F4F4")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "#F4F4F4")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "#F4F4F4")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "#F4F4F4")
+        
+    def draw_move(self, to_cell, undo=False):
+        self_midpoint = Point((self._x1 + self._x2)//2, (self._y1 + self._y2)//2)
+        to_cell_midpoint = Point(((to_cell._x1 + to_cell._x2)//2), ((to_cell._y1 + to_cell._y2)//2))
+        if undo == False:
+            fill_colour = "#D32F2F"
+        else:
+            fill_colour = "#B0B0B0"
+        self._win.draw_line(Line(self_midpoint, to_cell_midpoint), fill_colour)
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(0, 0), Point(400, 300)), "#F4F4F4")
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell2.has_bottom_wall= False
+    cell3 = Cell(win)
+    cell3.has_bottom_wall=False
+    cell3.has_right_wall=False
+    cell4 = Cell(win)
+    cell4.has_top_wall=False
+    cell4.has_left_wall=False
+    cell5 = Cell(win)
+    cell1.draw(0, 0, 50, 50)       # Top-left cell
+    cell2.draw(50, 0, 100, 50)     # Top-right cell
+    cell3.draw(0, 50, 50, 100)     # Bottom-left cell
+    cell4.draw(50, 50, 100, 100)   # Bottom-right cell
+    cell5.draw(400, 400, 450, 450) # Separate cell
+    cell2.draw_move(cell4)
+    cell4.draw_move(cell3, undo=True)    
+    win.draw_line(Line(Point(0, 300), Point(400, 300)), "#F4F4F4")
     win.wait_for_close()
 
 main()
